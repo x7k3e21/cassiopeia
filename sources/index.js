6cc8cc94a20c62b053f08f304e702a80495d5418
x7k3e21 <uly4nkin.denis@gmail.com>
2024-11-24 00:59:30 +0300

    Image generation testing

--- sources/index.js
+++ sources/index.js
@@ -8,6 +8,39 @@ const client = new grammy.Bot(clientToken);
 
 client.command("start", clientCommandStart);
 
+const OpenAI = require("openai");
+
+const OpenAIClient = new OpenAI({
+    baseURL: "https://api.naga.ac/v1",
+    apiKey: process.env.OPENAI_TOKEN}); 
+
+client.command("drawSD", async (context) => {
+    const imagePrompt = context.message.text.split(" ");
+    imagePrompt.shift();
+    
+    let OpenAIResponse = await OpenAIClient.images.generate({model:"sdxl", prompt:imagePrompt.toString()});
+
+    await context.replyWithPhoto(OpenAIResponse.data[0].url);
+});
+
+client.command("drawKD", async (context) => {
+    const imagePrompt = context.message.text.split(" ");
+    imagePrompt.shift();
+    
+    let OpenAIResponse = await OpenAIClient.images.generate({model:"kandinsky-3.1", prompt:imagePrompt.toString()});
+
+    await context.replyWithPhoto(OpenAIResponse.data[0].url);
+});
+
+client.command("drawPG", async (context) => {
+    const imagePrompt = context.message.text.split(" ");
+    imagePrompt.shift();
+    
+    let OpenAIResponse = await OpenAIClient.images.generate({model:"playground-v2.5", prompt:imagePrompt.toString()});
+
+    await context.replyWithPhoto(OpenAIResponse.data[0].url);
+});
+
 async function clientCommandStart(context) {
     await context.reply("Hello, world!");
 }
@@ -18,7 +51,7 @@ const application = express();
 
 application.use(express.json());
 
-application.use(grammy.webhookCallback(client, "express"));
+//application.use(grammy.webhookCallback(client, "express"));
 
 application.get("/", (request, response) => {
     response.send("<h1>Hello, world!</h1>");
@@ -32,3 +65,5 @@ function serverCallback() {
 
     console.log(`${serverAddress}:${serverPort}`);
 }
+
+client.start();
