dfd3d275cba01c58cd7f85caf5851f7494799af3
x7k3e21 <uly4nkin.denis@gmail.com>
2024-12-27 00:26:37 +0300

    Boilerplate rewrite

--- sources/index.js
+++ sources/index.js
@@ -1,34 +1,40 @@
 
 const grammy = require("grammy");
+const express = require("express");
 
-const clientToken = process.env.BOT_TOKEN;
-const clientPort = process.env.BOT_PORT;
+const clientToken = process.env.TELEGRAM_TOKEN;
+
+const clientAddress = process.env.CLIENT_ADDRESS;
+const clientPort = process.env.CLIENT_PORT;
 
 const client = new grammy.Bot(clientToken);
 
-client.command("start", clientCommandStart);
+const clientENV = process.env.NODE_ENV;
 
-async function clientCommandStart(context) {
-    await context.reply("Hello, world!");
+if (clientENV == "release") {
+    const application = express();
+
+    application.use(express.json());
+
+    const clientRoute = `/${clientToken}`;
+    const clientWebhook = grammy.webhookCallback(client, "express");
+
+    application.use(clientRoute, clientWebhook);
+
+    const server = application.listen(clientPort, serverListener);
+
+    async function serverListener() {
+        const serverAddress = server.address().address;
+        const serverPort = server.address().port;
+
+        await client.api.setWebhook(`https://${clientAddress}:${clientPort}${clientRoute}`);
+    }
 }
 
-const express = require("express");
+if (clientENV == "debug") {
+    const clientPollingOptions = {
+        drop_pending_updates: true
+    };
 
-const application = express();
-
-application.use(express.json());
-
-application.use(grammy.webhookCallback(client, "express"));
-
-application.get("/", (request, response) => {
-    response.send("<h1>Hello, world!</h1>");
-});
-
-const server = application.listen(clientPort, serverCallback);
-
-function serverCallback() {
-    const serverAddress = server.address().address;
-    const serverPort = server.address().port;
-
-    console.log(`${serverAddress}:${serverPort}`);
+    client.start(clientPollingOptions);
 }
